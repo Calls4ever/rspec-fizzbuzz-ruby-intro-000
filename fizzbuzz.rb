c5f833173a0465c12bb1e6e274d05f4456b54aac
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-09 02:12:22 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
-def fizzbuzz(num)
-  if num%3==0
+def fizzbuzz(int)
+  if int%3==0
      "fizz"
-  elsif num%5==0
+  elsif int%5==0
      "buzz"
-  elsif num%3==0 && num%5==0
+  elsif int%3==0 && num%5==0
      "fizzbuzz"
 
 end
