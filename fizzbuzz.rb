70e4a6dd12454f4d7e4a377d728cbabf93924ef7
Fnu Tsering Norbu <thetseringnorbu@gmail.com>
2020-04-09 02:10:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   if num%3==0
-    puts "fizz"
+     "fizz"
   elsif num%5==0
-    puts "buzz"
+     "buzz"
   elsif num%3==0 && num%5==0
-    puts "fizzbuzz"
+     "fizzbuzz"
 
 
   end
